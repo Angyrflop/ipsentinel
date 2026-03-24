5f731294e768c917b3fbf1aa2d736e5fd5b4c83f
Angyrflop <justanyordinarydude@gmail.com>
2026-03-24 21:42:20 +0100

    Deleted unused varibles

--- include/config_ip.h
+++ include/config_ip.h
@@ -1,8 +1,6 @@
 #ifndef CONFIG_IP_H
 #define CONFIG_IP_H
     #include <stdint.h>
-    #define MAX_CALLCOUNT  2000
-    #define START_CAPACITY  4
     #define IP_FILE_PATH "ips.bin"
     static const uint64_t MAGIC = 0x0123456789ABCDEFULL;
 #endif  /*CONFIG_IP_H*/
